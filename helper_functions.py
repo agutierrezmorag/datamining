056337178afb07efbfadefd7077c02f1977c919e
alvarog <alvarogutierrezmoraga@gmail.com>
2024-06-18 21:52:35 -0400

    added summarize_df function

--- helper_functions.py
+++ helper_functions.py
@@ -1,4 +1,54 @@
 import numpy as np
+import pandas as pd
+
+
+def summarize_dataframe(df):
+    summary_data = []
+
+    for col in df.columns:
+        data_type = df[col].dtype
+        # Simplify data type display
+        if data_type == "int64":
+            display_data_type = "int"
+        elif data_type == "float64":
+            display_data_type = "float"
+        elif data_type == "object":
+            display_data_type = "string"
+        else:
+            display_data_type = str(data_type)
+
+        non_null_count = df[col].notnull().sum()
+        null_count = df[col].isnull().sum()
+        most_repeated_value = (
+            df[col].value_counts().idxmax() if not df[col].isnull().all() else None
+        )
+
+        # Initialize lowest and highest values as "-"
+        lowest_value = "-"
+        highest_value = "-"
+
+        # Check if the column data type is numeric to calculate min and max
+        if pd.api.types.is_numeric_dtype(data_type):
+            lowest_value = df[col].min()
+            highest_value = df[col].max()
+            # Adjust for non-numeric highest and lowest values
+            lowest_value = lowest_value if pd.notnull(lowest_value) else "-"
+            highest_value = highest_value if pd.notnull(highest_value) else "-"
+
+        summary_data.append(
+            {
+                "Columna": col,
+                "Tipo de dato": display_data_type,
+                "Valores válidos": non_null_count,
+                "Valores nulos": null_count,
+                "Moda": most_repeated_value if pd.notnull(most_repeated_value) else "-",
+                "Valor mínimo": lowest_value,
+                "Valor máximo": highest_value,
+            }
+        )
+
+    summary_df = pd.DataFrame(summary_data)
+    return summary_df
 
 
 def format_numbers(x):
